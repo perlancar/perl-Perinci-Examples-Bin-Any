f704deea085235c75184035e1dce9ea809b79fae
Steven Haryanto (on PC, Jakarta) <stevenharyanto@gmail.com>
2014-07-18 14:47:24 +0700

    Add FAQ about the apparent absence of Perinci::Examples::Bin::Lite

--- lib/Perinci/Examples/Bin/Any.pm
+++ lib/Perinci/Examples/Bin/Any.pm
@@ -13,4 +13,13 @@ use 5.010001;
 This distribution contains example scripts in C<bin/> which use
 L<Perinci::CmdLine::Any>.
 
+
+=head1 FAQ
+
+=head2 Where are examples using Perinci::CmdLine::Lite?
+
+You can use the scripts in this distribution, and run them with:
+
+ % PERINCI_CMDLINE_ANY=Perinci::CmdLine::Lite script ...
+
 =cut
