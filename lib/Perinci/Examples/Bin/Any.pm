f5cf62c7962a4c86d610322480813dc0c939c74f
Steven Haryanto (on PC, Jakarta) <stevenharyanto@gmail.com>
2014-07-18 13:24:19 +0700

    Initial commit

---
+++ lib/Perinci/Examples/Bin/Any.pm
@@ -0,0 +1,16 @@
+package Perinci::Examples::Bin::Any;
+
+# DATE
+# VERSION
+
+use 5.010001;
+
+1;
+# ABSTRACT: Perinci example scripts (using Perinci::CmdLine::Any)
+
+=head1 DESCRIPTION
+
+This distribution contains example scripts in C<bin/> which use
+L<Perinci::CmdLine::Any>.
+
+=cut
